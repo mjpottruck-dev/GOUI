c8dae9fc078988d6468b40ca078dbfd80e9a021b
mjpottruck-dev <mjpottruck@gmail.com>
2026-01-29 20:16:07 -0800

    Update water polo sport module

--- GOUI/WaterPoloSport.swift
+++ GOUI/WaterPoloSport.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct WaterPoloSport: SportDefinition {
-    let id = "water_polo"
+    let id = SportCatalog.waterPoloID
     let displayName = "Water Polo"
     let season: SportSeason = .spring
     let supportsTimer = true
@@ -24,11 +24,15 @@ struct WaterPoloSport: SportDefinition {
     let statSchema: [StatType] = [
         StatType(id: "goals", displayName: "Goals", shortLabel: "G", countsForTeam: true, countsForPlayer: true),
         StatType(id: "assists", displayName: "Assists", shortLabel: "A", countsForTeam: true, countsForPlayer: true),
+        StatType(id: "shots", displayName: "Shots", shortLabel: "S", countsForTeam: true, countsForPlayer: true),
         StatType(id: "exclusions", displayName: "Exclusions", shortLabel: "EX", countsForTeam: true, countsForPlayer: true),
-        StatType(id: "saves", displayName: "Saves", shortLabel: "SV", countsForTeam: true, countsForPlayer: true),
         StatType(id: "steals", displayName: "Steals", shortLabel: "STL", countsForTeam: true, countsForPlayer: true),
         StatType(id: "blocks", displayName: "Blocks", shortLabel: "BLK", countsForTeam: true, countsForPlayer: true),
-        StatType(id: "turnovers", displayName: "Turnovers", shortLabel: "TO", countsForTeam: true, countsForPlayer: true)
+        StatType(id: "turnovers", displayName: "Turnovers", shortLabel: "TO", countsForTeam: true, countsForPlayer: true),
+        StatType(id: "saves", displayName: "Saves", shortLabel: "SV", countsForTeam: true, countsForPlayer: true),
+        StatType(id: "goalsAllowed", displayName: "Goals Allowed", shortLabel: "GA", countsForTeam: true, countsForPlayer: true),
+        StatType(id: "pkSaved", displayName: "PK Saved", shortLabel: "PKS", countsForTeam: true, countsForPlayer: true),
+        StatType(id: "pkAllowed", displayName: "PK Allowed", shortLabel: "PKA", countsForTeam: true, countsForPlayer: true)
     ]
 
     let eventTypes: [EventType] = [
@@ -41,8 +45,22 @@ struct WaterPoloSport: SportDefinition {
             isGoalieOnly: false,
             usesGoalie: false,
             uiAction: .assist,
-            primaryStatChanges: ["goals": 1],
+            primaryStatChanges: ["goals": 1, "shots": 1],
             secondaryStatChanges: ["assists": 1],
+            shotOutcomeStats: nil,
+            cardStatChanges: [:]
+        ),
+        EventType(
+            id: "shot",
+            label: "Shot",
+            iconName: "scope",
+            requiresPlayer: true,
+            secondaryPlayerOptional: false,
+            isGoalieOnly: false,
+            usesGoalie: false,
+            uiAction: .direct,
+            primaryStatChanges: ["shots": 1],
+            secondaryStatChanges: [:],
             shotOutcomeStats: nil,
             cardStatChanges: [:]
         ),
@@ -106,7 +124,7 @@ struct WaterPoloSport: SportDefinition {
             id: "save",
             label: "Save",
             iconName: "hand.raised.fill",
-            requiresPlayer: false,
+            requiresPlayer: true,
             secondaryPlayerOptional: false,
             isGoalieOnly: true,
             usesGoalie: true,
@@ -117,15 +135,15 @@ struct WaterPoloSport: SportDefinition {
             cardStatChanges: [:]
         ),
         EventType(
-            id: "opponentGoal",
-            label: "Opponent Goal",
+            id: "goalAllowed",
+            label: "Goal Allowed",
             iconName: "xmark.seal.fill",
-            requiresPlayer: false,
+            requiresPlayer: true,
             secondaryPlayerOptional: false,
-            isGoalieOnly: false,
-            usesGoalie: false,
+            isGoalieOnly: true,
+            usesGoalie: true,
             uiAction: .direct,
-            primaryStatChanges: [:],
+            primaryStatChanges: ["goalsAllowed": 1],
             secondaryStatChanges: [:],
             shotOutcomeStats: nil,
             cardStatChanges: [:]
@@ -138,7 +156,7 @@ struct WaterPoloSport: SportDefinition {
         scoreLabel: "Goals",
         primaryStatID: "goals",
         teamEventPoints: ["goal": 1],
-        opponentEventPoints: ["opponentGoal": 1],
+        opponentEventPoints: ["goalAllowed": 1],
         periodEventPoints: [:],
         opponentPeriodEventPoints: [:]
     )
